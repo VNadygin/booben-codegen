b9bb59f0195bd9b3804470c4baf996b7616d4d91
Vladimir Nadygin <VNadygin@me.com>
2017-09-28 20:01:44 +0300

    const and routeParams

--- test.js
+++ test.js
@@ -152,3 +152,198 @@ test('generate jsx with object prop', t => {
   t.is(code, expected)
 })
 
+test('generate const jsx with prop={false}', t => {
+  const jssy = {
+    "id": 0,
+    "name": "App",
+    "title": "",
+    "isWrapper": false,
+    "props": {
+      "prop": {
+        "source": "const",
+        "sourceData": {
+          "value": false
+        }
+      }
+    },
+    "children": []
+  }
+  const expected = '<App prop={false}></App>;'
+  const ast = generateJSX(jssy)
+  const { code } = generate(ast)
+  t.is(code, expected)
+})
+
+test('generate const jsx with prop="test string"', t => {
+  const jssy = {
+    "id": 0,
+    "name": "App",
+    "title": "",
+    "isWrapper": false,
+    "props": {
+      "prop": {
+        "source": "const",
+        "sourceData": {
+          "value": "test string"
+        }
+      }
+    },
+    "children": []
+  }
+  const expected = '<App prop="test string"></App>;'
+  const ast = generateJSX(jssy)
+  const { code } = generate(ast)
+  t.is(code, expected)
+})
+
+test('generate const jsx with prop="test string"', t => {
+  const jssy = {
+    "id": 0,
+    "name": "App",
+    "title": "",
+    "isWrapper": false,
+    "props": {
+      "prop": {
+        "source": "const",
+        "sourceData": {
+          "value": {
+            "property1": "foo",
+            "property2": "bar",
+          }
+        }
+      }
+    },
+    "children": []
+  }
+  const expected = '<App prop={{\n  property1: "foo",\n  property2: "bar"\n}}></App>;'
+  const ast = generateJSX(jssy)
+  const { code } = generate(ast)
+  t.is(code, expected)
+})
+
+test('generate const jsx with prop=[array]', t => {
+  const jssy = {
+    "id": 0,
+    "name": "App",
+    "title": "",
+    "isWrapper": false,
+    "props": {
+      "prop": {
+        "source": "const",
+        "sourceData": {
+          "value": [1,2,3],
+        }
+      }
+    },
+    "children": []
+  }
+  const expected = '<App prop={[1, 2, 3]}></App>;'
+  const ast = generateJSX(jssy)
+  const { code } = generate(ast)
+  t.is(code, expected)
+})
+
+test('generate const jsx with nested object', t => {
+  const jssy = {
+    "id": 0,
+    "name": "App",
+    "title": "",
+    "isWrapper": false,
+    "props": {
+      "prop": {
+        "source": "const",
+        "sourceData": {
+          "value": {
+            "foo": "bar",
+            "nested": {
+              "a": 1
+            }
+          }
+        }
+      }
+    },
+    "children": []
+  }
+  const expected = '<App prop={{\n  foo: "bar",\n  nested: {\n    a: 1\n  }\n}}></App>;'
+  const ast = generateJSX(jssy)
+  const { code } = generate(ast)
+  t.is(code, expected)
+})
+
+test('generate const jsx const and static jssyValue', t => {
+  const jssy = {
+    "id": 0,
+    "name": "App",
+    "title": "",
+    "isWrapper": false,
+    "props": {
+      "prop1": {
+        "source": "const",
+        "sourceData": {
+          "value": 5
+        }
+      },
+      "prop2": {
+        "source": "static",
+        "sourceData": {
+          "value": {
+            "property1": { "source": "static", "sourceData": { "value": "foo" } },
+          }
+        }
+      }
+    },
+    "children": []
+  }
+  const expected = '<App prop1={5} prop2={{\n  property1: "foo"\n}}></App>;'
+  const ast = generateJSX(jssy)
+  const { code } = generate(ast)
+  t.is(code, expected)
+})
+
+test('generate paramRoutes', t => {
+  const jssy = {
+    "id": 0,
+    "name": "App",
+    "title": "",
+    "isWrapper": false,
+    "props": {
+      "prop": {
+        "source": "routeParams", 
+        "sourceData": {
+          "routeId": 1,
+          "paramName": "foo"
+        }
+      }
+    },
+    "children": []
+  }
+  const expected = '<App prop={props.match.params.foo}></App>;'
+  const ast = generateJSX(jssy)
+  const { code } = generate(ast)
+  t.is(code, expected)
+})
+
+test('generate const jsx static and routeParams jssyValue', t => {
+  const jssy = {
+    "id": 0,
+    "name": "App",
+    "title": "",
+    "isWrapper": false,
+    "props": {
+      "prop": {
+        "source": "static",
+        "sourceData": {
+          "value": {
+            "foo": { "source": "static", "sourceData": { "value": 42 } },
+            "bar": { "source": "routeParams", "sourceData": { "routeId": 1, "paramName": "foo" } }
+          }
+        }
+      }
+    },
+    "children": []
+  }
+  const expected = '<App prop={{\n  foo: 42,\n  bar: props.match.params.foo\n}}></App>;'
+  const ast = generateJSX(jssy)
+  const { code } = generate(ast)
+  t.is(code, expected)
+})
