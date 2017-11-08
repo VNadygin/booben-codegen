556fb7308324db5b8d7ac766211d44f740ac698c
Vladimir Nadygin <VNadygin@me.com>
2017-11-08 15:10:02 +0300

    code style

--- bin/cli.js
+++ bin/cli.js
@@ -2,30 +2,30 @@
 const minimist = require('minimist');
 const path = require('path');
 const fs = require('mz/fs');
-const generate = require('../lib/generate')
+const generate = require('../lib/generate');
 
 
 const cli = async () => {
   const argv = minimist(process.argv.slice(2));
   const libraryPath = path.resolve(process.cwd());
 
-  if (!(await fs.exists(libraryPath))) {
+  if (!(await fs.exists(libraryPath)))
     throw new Error(`${libraryPath} does not exist`);
-  }
+  
 
   const stats = await fs.stat(libraryPath);
   
-  if (!stats.isDirectory()) {
+  if (!stats.isDirectory())
     throw new Error(`${libraryPath} is not a directory`);
-  }
   
-  const result = await fs.readFile(argv.f)
-  const jssyProject = JSON.parse(result)
-  await generate(jssyProject, argv.o)
-}
+  
+  const result = await fs.readFile(argv.f);
+  const jssyProject = JSON.parse(result);
+  await generate(jssyProject, argv.o);
+};
 
 cli()
   .catch(err => {
-    console.error(err)
-  })
+    console.error(err);
+  });
 
