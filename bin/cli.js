2260bfb1631a164af0b0d2ca469fdad6eb71e93a
Dmitriy Bizyaev <dmitriy.bizyaev@gmail.com>
2017-11-22 01:57:17 +0300

    refactored code for model building stage

--- bin/cli.js
+++ bin/cli.js
@@ -4,28 +4,25 @@ const path = require('path');
 const fs = require('mz/fs');
 const generate = require('../lib/generate');
 
-
 const cli = async () => {
   const argv = minimist(process.argv.slice(2));
   const libraryPath = path.resolve(process.cwd());
 
-  if (!(await fs.exists(libraryPath)))
+  if (!await fs.exists(libraryPath)) {
     throw new Error(`${libraryPath} does not exist`);
-  
+  }
 
   const stats = await fs.stat(libraryPath);
-  
-  if (!stats.isDirectory())
+
+  if (!stats.isDirectory()) {
     throw new Error(`${libraryPath} is not a directory`);
-  
-  
+  }
+
   const result = await fs.readFile(argv.f);
   const jssyProject = JSON.parse(result);
   await generate(jssyProject, argv.o);
 };
 
-cli()
-  .catch(err => {
-    console.error(err);
-  });
-
+cli().catch(err => {
+  console.error(err);
+});
