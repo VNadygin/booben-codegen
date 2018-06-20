e8ffff23242984a8746538baeee7213ff97ef26e
Vladimir Nadygin <vnadygin@me.com>
2018-06-20 16:16:46 +0300

    0.1.0

--- bin/cli.js
+++ bin/cli.js
@@ -6,8 +6,9 @@ const { generateProject } = require('../lib/api');
 const cli = async () => {
   const argv = minimist(process.argv.slice(2));
   const result = await fs.readFile(argv.f);
+  const meta = await fs.readFile(argv.m);
   const boobenProject = JSON.parse(result);
-  await generateProject(boobenProject, argv.o, { logger: console });
+  await generateProject(boobenProject, argv.o, meta, { logger: console });
 };
 
 cli().catch(err => {
